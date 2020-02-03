4781b89b23a946534530518955346754ed78077c
jln t-suzuki <jln@jlnnoiMac.local>
2020-02-03 18:52:43 +0900

    Create Login Compornents ログインフォーマットのコンポーネントを作成

---
+++ src/webnuxt/components/login/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid>
+        <div>
+          <p>test</p>
+        </div>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+      overlay: false,
+      dialog: {
+        alert: false
+      }
+    }
+  }
+}
+</script>
---
+++ src/webnuxt/pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <Login v-bind:devFlag="devFlag" />
+</template>
+
+<script>
+import Login from '~/components/login/login.vue'
+
+export default {
+  components: {
+    Login
+  },
+  data () {
+    return { devFlag: null }
+  },
+  asyncData ({ isDev }) {
+    return { devFlag: isDev }
+  }
+}
+</script>
